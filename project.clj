f8abac7e8f4aac371c954aa609fba817ef01fc0c
hairybreeches <dc345@cantab.net>
2014-03-06 11:36:47 +0000

    code from ch3

--- project.clj
+++ project.clj
@@ -2,11 +2,13 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.reader "0.8.3"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.4"]
                  [ring-server "0.3.0"]
                  [liberator "0.10.0"]
-                 [cheshire "5.2.0"]]
+                 [cheshire "5.2.0"]
+                 [lib-noir "0.7.2"]]
   :plugins [[lein-ring "0.8.7"]]
   :ring {:handler liberator-service.handler/app
          :init liberator-service.handler/init
